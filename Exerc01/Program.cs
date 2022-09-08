b272b53a68d7f095d3da46781aa5aec704be2ed9
Raquel Stefany <raquelstefany.s@hotmail.com>
2022-09-07 22:30:17 -0300

    Atividade C#

--- Exerc01/Program.cs
+++ Exerc01/Program.cs
@@ -10,65 +10,77 @@ namespace Exerc01
     {
         static void Main(string[] args)
         {
-            string nomeAluno;
+            string nomeAluno, fimPrograma = null;
             double nota1, nota2, media;
 
-            Console.WriteLine("Digite o nome do Aluno:");
-            nomeAluno = Console.ReadLine();
-
-            Console.WriteLine("");
-
-            Console.WriteLine("Digite a 1ª nota:");
-            nota1 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("");
-
-            Console.WriteLine("Digite a 2ª nota:");
-            nota2 = double.Parse(Console.ReadLine());
-
-            media = (nota1 + nota2) / 2;
-
-            if (media < 6.0)
+            do
             {
-                double nota3;
+                Console.WriteLine("Digite o nome do Aluno:");
+                nomeAluno = Console.ReadLine();
 
                 Console.WriteLine("");
 
-                Console.WriteLine("A Média Aritmética é " + media);
-                Console.WriteLine("");
-                Console.WriteLine("Digite a 3ª nota:");
-                nota3 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Digite a 1ª nota:");
+                nota1 = double.Parse(Console.ReadLine());
 
-                if (nota1 > nota2)
+                Console.WriteLine("");
+
+                Console.WriteLine("Digite a 2ª nota:");
+                nota2 = double.Parse(Console.ReadLine());
+
+                media = (nota1 + nota2) / 2;
+
+                if (media < 6.0)
                 {
-                    media = (nota1 + nota3) / 2;
+                    double nota3;
+
+                    Console.WriteLine("");
+
+                    Console.WriteLine("A Média Aritmética é " + media);
+                    Console.WriteLine("");
+                    Console.WriteLine("Digite a 3ª nota:");
+                    nota3 = double.Parse(Console.ReadLine());
+
+                    if (nota1 > nota2)
+                    {
+                        media = (nota1 + nota3) / 2;
+                    }
+                    else
+                    {
+                        media = (nota2 + nota3) / 2;
+                    }
+
+                    if (media > 6.0)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("A Média Aritmética é " + media);
+                        Console.WriteLine("Aprovado");
+                        Console.WriteLine("");
+                        //Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("A Média Aritmética é " + media);
+                        Console.WriteLine("Reprovado");
+                        Console.WriteLine("");
+                        //Console.ReadKey();
+                    }
                 }
                 else
                 {
-                    media = (nota2 + nota3) / 2;
-                }
-
-                if (media > 6.0)
-                {
-                    Console.WriteLine("");
                     Console.WriteLine("A Média Aritmética é " + media);
                     Console.WriteLine("Aprovado");
-                    Console.ReadKey();
-                }
-                else
-                {
                     Console.WriteLine("");
-                    Console.WriteLine("A Média Aritmética é " + media);
-                    Console.WriteLine("Reprovado");
-                    Console.ReadKey();
+                    //Console.ReadKey();
                 }
+
+                Console.WriteLine("Deseja encerrar o programa?");
+                fimPrograma = Console.ReadLine().ToUpper();
+                Console.WriteLine("");
             }
-            else
-            {
-                Console.WriteLine("A Média Aritmética é " + media);
-                Console.WriteLine("Aprovado");
-                Console.ReadKey();
-            }
+            while (fimPrograma != "S");
+            Console.ReadKey();
         }
     }
 }
---
+++ Exerc02/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeProduto, fimPrograma = null;
+            double qtdMercadoria = 0, precoMercadoria = 0, totalMercadoria = 0, precoVenda = 0;
+
+            do
+            {
+                Console.WriteLine("Insira o nome do Produto:");
+                nomeProduto = Console.ReadLine();
+
+                if (nomeProduto != "")
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Digite o preço da Mercadoria:");
+                    precoMercadoria = double.Parse(Console.ReadLine());
+
+                    if (precoMercadoria > 0)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Digite a quantidade de Mercadorias:");
+                        qtdMercadoria = double.Parse(Console.ReadLine());
+
+                        if (qtdMercadoria > 0)
+                        {
+                            totalMercadoria = (precoMercadoria * 0.3) + (precoMercadoria * qtdMercadoria);
+                            precoVenda = totalMercadoria + (totalMercadoria * 0.3);
+
+                            Console.WriteLine("");
+                            Console.WriteLine("O valor total da Mercadoria é: " + totalMercadoria);
+                            Console.WriteLine("o preço de venda da Mercadoria é: " + precoVenda);
+
+                            Console.WriteLine("");
+                            Console.WriteLine("Deseja encerrar o programa?\nS - Sim\nN - Não\n");
+                            fimPrograma = Console.ReadLine().ToUpper();
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Digite a quantidade de Mercadorias:");
+                            qtdMercadoria = double.Parse(Console.ReadLine());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Digite o preço da Mercadoria:");
+                        precoMercadoria = double.Parse(Console.ReadLine());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Insira o nome do Produto:");
+                    nomeProduto = Console.ReadLine();
+                }
+            }
+            while (fimPrograma != "S");
+            Console.ReadKey();
+        }
+    }
+}
